0b237a162d72648c42e021c2e1a7d8b77eab5472
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-19 18:14:54 +0300

    Adds contracts and authors methods to Book class

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -24,6 +24,12 @@ class Book:
     def __init__(self, title):
         self.title = title
         Book.all.append(self)
+    
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+
+    def authors(self):
+        return list(set(contract.author for contract in self.contracts()))
 
 
 class Contract:
