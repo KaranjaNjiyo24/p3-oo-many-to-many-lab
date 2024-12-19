589c586cf795864b69121997553111b528717eff
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-19 18:03:09 +0300

    Adds contract class with author, book, date and royalties attributes

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -15,4 +15,21 @@ class Book:
 
 
 class Contract:
-    pass
+    all =[]
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("Author must be an instance of Author class")
+        if not isinstance(book, Book):
+            raise Exception("Book must be an instance of Book class")
+        if not isinstance(date, str):
+            raise Exception("Date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("Royalties must be an integer")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+        
